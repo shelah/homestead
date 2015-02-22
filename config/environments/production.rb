7a136cf366f20cf78bb8dd668641658dbcff1edb
Shelah Horvitz <shelah@shelahhorvitz.com>
2015-02-22 12:38:38 -0500

    Added ember support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Ember support
+  config.ember.variant = :production
 end
